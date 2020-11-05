59a2eeaf519334d8034f54f1ce44b2008cb47e74
Jon <bedardjo@gmail.com>
2020-11-05 09:02:49 -0500

    simplify as much as possible

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,55 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:snakewich/model/direction.dart';
-import 'package:snakewich/views/snake-board.dart';
 
 void main() {
   runApp(SnakeApp());
 }
 
 class SnakeApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'SNAAAKE',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
       home: SnakeGame(),
     );
   }
+}
+
+enum Direction { north, east, south, west }
+
+extension DirectionUtil on Direction {
+  bool isOrthogonal(Direction d) {
+    switch (this) {
+      case Direction.north:
+      case Direction.south:
+        return d == Direction.east || d == Direction.west;
+      case Direction.west:
+      case Direction.east:
+        return d == Direction.south || d == Direction.north;
+    }
+  }
+}
+
+class Coord {
+  final int x;
+  final int y;
+
+  Coord(this.x, this.y);
+
+  Coord getNeighbor(Direction d) => Coord(
+      d == Direction.east
+          ? x + 1
+          : d == Direction.west
+              ? x - 1
+              : x,
+      d == Direction.north
+          ? y - 1
+          : d == Direction.south
+              ? y + 1
+              : y);
+  bool operator ==(obj) => obj is Coord && obj.x == x && obj.y == y;
+  int get hashCode => x * 31 + y;
 }
 
 class SnakeGame extends StatefulWidget {
@@ -32,9 +61,9 @@ class SnakeGame extends StatefulWidget {
 class SnakeGameState extends State<SnakeGame> {
   Random r = new Random();
 
-  SnakeBoard board;
-  List<SnakeBoardTile> snake;
-  List<SnakeBoardTile> apples;
+  int width;
+  int height;
+  List<Coord> snake;
   Direction snakeDirection;
 
   List<Direction> swipes;
@@ -42,6 +71,7 @@ class SnakeGameState extends State<SnakeGame> {
   Timer updateTimer;
 
   int appleCountdown;
+  List<Coord> apples;
 
   @override
   void initState() {
@@ -50,8 +80,9 @@ class SnakeGameState extends State<SnakeGame> {
   }
 
   void _reset() {
-    board = SnakeBoard(20, 20);
-    snake = [board.tiles[5][3], board.tiles[5][2]];
+    width = 20;
+    height = 20;
+    snake = [Coord(5, 3), Coord(4, 3)];
     apples = [];
     snakeDirection = Direction.east;
     swipes = [];
@@ -65,27 +96,35 @@ class SnakeGameState extends State<SnakeGame> {
         snakeDirection = swipes.first;
         swipes.removeAt(0);
       }
-      SnakeBoardTile newHead = snake.first.getNeighbor(snakeDirection);
-      int tailLength = snake.length - 1;
-      SnakeBoardTile consumedApple = apples
-          .firstWhere((element) => element == newHead, orElse: () => null);
-      if (consumedApple != null) {
-        tailLength++;
-        apples.remove(consumedApple);
-      }
-      snake = [newHead] + snake.take(tailLength).toList();
-      appleCountdown--;
-      if (appleCountdown == 0) {
-        appleCountdown = 4 + r.nextInt(10);
-        List<SnakeBoardTile> eligibleApplePositions =
-            board.tiles.expand((e) => e).toList();
-        snake.forEach((element) {
-          eligibleApplePositions.remove(element);
-        });
-        apples.add(
-            eligibleApplePositions[r.nextInt(eligibleApplePositions.length)]);
-      }
+      _updateSnake();
+      _updateApples();
     });
+  }
+
+  void _updateSnake() {
+    Coord newHead = snake.first.getNeighbor(snakeDirection);
+    int tailLength = snake.length - 1;
+    Coord consumedApple =
+        apples.firstWhere((element) => element == newHead, orElse: () => null);
+    if (consumedApple != null) {
+      tailLength++;
+      apples.remove(consumedApple);
+    }
+    snake = [newHead, ...snake.take(tailLength)];
+  }
+
+  void _updateApples() {
+    appleCountdown--;
+    if (appleCountdown <= 0) {
+      List<Coord> eligibleApplePositions =
+          List.generate(height, (y) => List.generate(width, (x) => Coord(x, y)))
+              .expand((e) => e)
+              .where((e) => !snake.contains(e) && !apples.contains(e))
+              .toList();
+      apples.add(
+          eligibleApplePositions[r.nextInt(eligibleApplePositions.length)]);
+      appleCountdown = 4 + r.nextInt(10);
+    }
   }
 
   void _swipe(Direction direction) {
@@ -115,16 +154,58 @@ class SnakeGameState extends State<SnakeGame> {
             }
           },
           child: AspectRatio(
-            aspectRatio: board.width / board.height,
-            child: Stack(children: <Widget>[
-              Positioned.fill(
-                  child: CustomPaint(
-                      painter: SnakePainter(
-                          board: board, snake: snake, apples: apples)))
+            aspectRatio: width / height,
+            child: Stack(fit: StackFit.expand, children: <Widget>[
+              CustomPaint(
+                  painter: SnakePainter(
+                      width: width,
+                      height: height,
+                      snake: snake,
+                      apples: apples))
             ]),
           ),
         ),
       ),
     );
   }
+}
+
+class SnakePainter extends CustomPainter {
+  final int width;
+  final int height;
+  final List<Coord> snake;
+  final List<Coord> apples;
+
+  SnakePainter({this.width, this.height, this.snake, this.apples});
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint tileColor = Paint()..color = Colors.blue.shade100;
+    Paint snakeColor = Paint()..color = Colors.green.shade800;
+    Paint appleColor = Paint()..color = Colors.red.shade800;
+    double tileSize = size.width / width;
+    for (int y = 0; y < height; y++) {
+      for (int x = 0; x < width; x++) {
+        drawTile(canvas, x, y, tileSize, .5, tileColor);
+      }
+    }
+    for (Coord snake in snake) {
+      drawTile(canvas, snake.x, snake.y, tileSize, 2, snakeColor);
+    }
+    for (Coord apple in apples) {
+      drawTile(canvas, apple.x, apple.y, tileSize, 5, appleColor);
+    }
+  }
+
+  void drawTile(
+      Canvas canvas, int x, int y, double size, double deflation, Paint p) {
+    canvas.drawRect(
+        Rect.fromLTWH(x * size, y * size, size, size).deflate(deflation), p);
+  }
+
+  @override
+  bool shouldRepaint(SnakePainter old) =>
+      old.height != height ||
+      old.width != width ||
+      old.snake != snake ||
+      old.apples != apples;
 }
